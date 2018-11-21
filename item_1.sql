e77cdb80031a767d3aa705d53366d859e53c3f84
jvrobinho <jvmonteirorj@gmail.com>
2018-11-20 22:09:36 -0200

    Comando consertado

--- item_1.sql
+++ item_1.sql
@@ -1,3 +1,1 @@
-select a.index_name, a.table_name, b.column_name
-from user_indexes a, user_cons_columns b
-where a.index_name = b.constraint_name;
+select index_name, table_name, column_name from user_ind_columns;
